e30d7e9032231f9b8c1295105415063224354796
Rayyan-Uddin <rayyanuddin007@gmail.com>
2023-02-27 12:53:21 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,118 @@
+// Question 1
+
+// var input = prompt("Write a Character:");
+
+// if (input == input.charCodeAt(65)){
+//     alert("The Enter Value is Upper Case!")
+// }
+
+// else if ( input == input.charCodeAt(90)){
+//     alert("The Enter Value is a Number!")
+// }
+// else if ( input == input.charCodeAt(97)){
+//     alert("The Enter Value is a Lower Case!")
+// }
+// ------------------------------------------
+
+// Question 2
+
+// var value1 = +prompt("Enter a Value 1:")
+// var value2 = +prompt("Enter a Value 2:")
+
+// if (value1 > value2){
+//     alert(value1);
+// } 
+// else if ( value2 > value1){
+//     alert(value2);
+// }
+
+// ---------------------------------------------
+
+// Question 3
+
+// var value = +prompt("Enter a Value :")
+
+// if (value < 0){
+//     alert("Entered Value is Negative!")
+// }
+// if (value === 0){
+//     alert("Entered Value is Zero!")
+// }
+// if (value > 0){
+//     alert("Entered Value is Positive!")
+// }
+
+// --------------------------------------------
+
+// Quesiton 4
+
+// var char = prompt("Enter a Character:");
+
+// if ( char === "a"){
+//     alert("True")
+// }
+// else if ( char === "e"){
+//     alert("True")
+// }
+// else if ( char === "i"){
+//     alert("True")
+// }
+// else if ( char === "o"){
+//     alert("True")
+// }
+// else if ( char === "u"){
+//     alert("True")
+// }
+// else {
+//     alert("False")
+// }
+
+// ------------------------------------------
+
+// Question 5
+
+// var pass1 = "rayyan.123";
+// var pass2 = prompt("Enter your Password:")
+
+// if (pass1 == pass2){
+//     alert("Correct! The password you entered matches the original password")
+// }
+// else{
+//     alert("Incorrect password")
+// }
+
+// -----------------------------------------------------------
+
+// Question 6
+
+// var greeting;
+
+// var hour = prompt("Enter an Hour:")
+
+// if (hour < 18){
+//     greeting = "Good day";
+//     alert(greeting)
+// }
+// else{
+//     greeting = "Good Evening";
+//     alert(greeting)
+// }
+
+// ---------------------------------------------
+
+// Question 7
+
+// var time = prompt("Enter time in 24 hour format. eg ('1900'):");
+
+// if (time >= 0000 && time < 1200){
+//     alert("Good morning!")
+// }
+// else if (time >= 1200 && time < 1700){
+//     alert("Good Afternoon!")
+// }
+// else if (time >= 1700 && time < 2100){
+//     alert("Good Evening!")
+// }
+// else if (time >= 2100 && time < 2359){
+//     alert("Good Night!")
+// }
